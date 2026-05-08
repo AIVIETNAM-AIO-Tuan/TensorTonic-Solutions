acf6b895f307219e5df146ac0db61b5c555c201e
NGÔ ĐỨC TUẤN <147224723+AIVIETNAM-AIO-Tuan@users.noreply.github.com>
2026-05-09 01:37:16 +0700

    Update solution for global-avg-pooling

--- global-avg-pooling/global-avg-pooling.py
+++ global-avg-pooling/global-avg-pooling.py
@@ -5,36 +5,43 @@ def global_avg_pool(x):
     Compute global average pooling over spatial dims.
     Supports (C,H,W) => (C,) and (N,C,H,W) => (N,C).
     """
+    # x = np.array(x)
+    # if len(x.shape) == 4:
+    #   N = x.shape[0]
+    #   C = x.shape[1]
+    #   H = x.shape[2]
+    #   W = x.shape[3]
+    #   print(N,C,H,W)
+    #   gap = np.empty((N, C))
+    #   for n in range(N):
+    #     for c in range(C):
+    #       gap_c = 0
+    #       for h in range(H):
+    #         for w in range(W):
+    #             gap_c += x[n,c,h,w]
+    #       gap_c = gap_c/(H*W)
+    #       gap[n,c] =  gap_c
+    # elif len(x.shape) == 3:
+    #   C = x.shape[0]
+    #   H = x.shape[1]
+    #   W = x.shape[2]
+    #   print(C,H,W)
+    #   gap = np.empty((C,)) 
+    #   for c in range(C):
+    #       gap_c = 0
+    #       for h in range(H):
+    #         for w in range(W):
+    #             gap_c += x[c,h,w]
+    #       gap_c = gap_c/(H*W)
+    #       gap[c] =  gap_c
+    # else:
+    #     raise ValueError
+    # return gap
+
     x = np.array(x)
-    if len(x.shape) == 4:
-      N = x.shape[0]
-      C = x.shape[1]
-      H = x.shape[2]
-      W = x.shape[3]
-      print(N,C,H,W)
-      gap = np.empty((N, C))
-      for n in range(N):
-        for c in range(C):
-          gap_c = 0
-          for h in range(H):
-            for w in range(W):
-                gap_c += x[n,c,h,w]
-          gap_c = gap_c/(H*W)
-          gap[n,c] =  gap_c
-    elif len(x.shape) == 3:
-      C = x.shape[0]
-      H = x.shape[1]
-      W = x.shape[2]
-      print(C,H,W)
-      gap = np.empty((C,)) 
-      for c in range(C):
-          gap_c = 0
-          for h in range(H):
-            for w in range(W):
-                gap_c += x[c,h,w]
-          gap_c = gap_c/(H*W)
-          gap[c] =  gap_c
-    else:
+    if len(x.shape)<3:
         raise ValueError
-    return gap
+    spatial_axes = (2, 3) if x.ndim == 4 else (1, 2)
+    
+    return np.mean(x, axis=spatial_axes)
     
