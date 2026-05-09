6146eb02eb5266f90b3e39f1b86b71d3ee637707
NGÔ ĐỨC TUẤN <147224723+AIVIETNAM-AIO-Tuan@users.noreply.github.com>
2026-05-09 23:52:05 +0700

    Update solution for gradient-clipping

--- gradient-clipping/gradient-clipping.py
+++ gradient-clipping/gradient-clipping.py
@@ -7,24 +7,28 @@ def clip_gradients(g, max_norm):
     g = np.array(g,dtype = float)
     if max_norm <= 0:
         return g
-    v = g.flatten()
-    total = 0
-    for i in v:
-      total += i**2
-    norm = np.sqrt(total)
-    if len(g.shape) == 1:
-        for i in range(len(g)):
-            if norm <= max_norm:
-              g[i] = g[i]
-            else:
-              g[i] = (max_norm/norm)*g[i]
-    else:
-        H = g.shape[-2:][0]
-        W = g.shape[-2:][1]
-        for h in range(H):
-          for w in range(W):
-            if norm <= max_norm:
-              g[...,h,w] = g[...,h,w]
-            else:
-              g[...,h,w] = (max_norm/norm)*g[...,h,w]
+    # v = g.flatten()
+    # total = 0
+    # for i in v:
+    #   total += i**2
+    # norm = np.sqrt(total)
+    # if len(g.shape) == 1:
+    #     for i in range(len(g)):
+    #         if norm <= max_norm:
+    #           g[i] = g[i]
+    #         else:
+    #           g[i] = (max_norm/norm)*g[i]
+    # else:
+    #     H = g.shape[-2:][0]
+    #     W = g.shape[-2:][1]
+    #     for h in range(H):
+    #       for w in range(W):
+    #         if norm <= max_norm:
+    #           g[...,h,w] = g[...,h,w]
+    #         else:
+    #           g[...,h,w] = (max_norm/norm)*g[...,h,w]
+
+    norm = np.linalg.norm(g)
+    if norm > max_norm:
+        g *= (max_norm/norm)
     return g
