71f33e553f6c18376c1882a681d9414dbe84ba07
NGÔ ĐỨC TUẤN <147224723+AIVIETNAM-AIO-Tuan@users.noreply.github.com>
2026-06-12 11:15:04 +0700

    Solve majority-classifier

--- majority-classifier/majority-classifier.py
+++ majority-classifier/majority-classifier.py
@@ -8,13 +8,7 @@ def majority_classifier(y_train, X_test):
     X_test = np.array(X_test)
     cls, count = np.unique(y_train, return_counts=True)
 
-    id = np.where(count == np.max(count))
-    id = int(id[0][0])
-    pred = cls[id]
-    
-    output = []
-    for i in range(len(X_test)):
-      output.append(pred)
-    
-    return output
+    pred = cls[np.argmax(count)]
+        
+    return np.full(len(X_test),pred).tolist()
     
