be281a9548eb623f3a2630551f8d2d6d5170b528
NGÔ ĐỨC TUẤN <147224723+AIVIETNAM-AIO-Tuan@users.noreply.github.com>
2026-07-22 19:26:46 +0700

    Solve transformers-tokenization

--- transformer/transformers-tokenization/transformers-tokenization.py
+++ transformer/transformers-tokenization/transformers-tokenization.py
@@ -22,54 +22,53 @@ class SimpleTokenizer:
         Build vocabulary from a list of texts.
         Add special tokens first, then unique words.
         """
+        #Split
         words = []
         for text in texts:
-          for word in text.lower().split():
+          for word in text.split():
               if word in words:
                 continue
               words.append(word) 
         sorted_words = sorted(words)
-
+        #word2id
         self.word_to_id[self.pad_token] = 0
         self.word_to_id[self.unk_token] = 1
         self.word_to_id[self.bos_token] = 2
         self.word_to_id[self.eos_token] = 3
-
-        for i in range(len(sorted_words)):
-          self.word_to_id[sorted_words[i]] = i+4
+        
+        for w in range(0, len(sorted_words)):
+          self.word_to_id[sorted_words[w]] = w + 4
 
         self.vocab_size = len(self.word_to_id)
-
+        
         self.id_to_word = {value:key for key,value in self.word_to_id.items()}
-
-
         return self.word_to_id, self.id_to_word
+          
     
     def encode(self, text: str) -> List[int]:
         """
         Convert text to list of token IDs.
         Use UNK for unknown words.
         """
-        text = text.lower()
-        token = text.split()
-        enc = []
-
-        for i in range(len(token)):
-          if token[i] in self.word_to_id.keys():
-            enc.append(self.word_to_id.get(token[i]))
+        #lowe and split
+        text = text.lower().split()
+        # self.build_vocab(text)
+        dic = self.word_to_id
+        print(dic.keys())
+        ids = []
+        for w in text:
+          if w in dic.keys():
+            print(w)
+            ids.append(dic[w])
           else:
-            enc.append(self.word_to_id.get(self.unk_token))
-
-
-        return enc
-
+            print(w)
+            ids.append(self.word_to_id.get(self.unk_token))
+        print(ids)
+        return ids
     
     def decode(self, ids: List[int]) -> str:
         """
         Convert list of token IDs back to text.
         """
-        dec = []
-        for id in ids:
-            dec.append(self.id_to_word.get(id, self.unk_token))
-        dec = " ".join(dec)
-        return dec
+        dic = self.id_to_word
+        return " ".join([dic.get(i, self.unk_token) for i in ids])
